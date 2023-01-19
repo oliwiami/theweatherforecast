df4d9494c5b8b5f66106e06aae2442a7d163b15d
Oliwia Mierkulow <79585505+olivemi@users.noreply.github.com>
2023-01-19 17:00:48 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App=() => {
 
   const API_KEY = process.env.REACT_APP_API_KEY
 
-  const [noData, setNoData] = useState('No Data Yet')
+  const [noData, setNoData] = useState('Search for location')
   const [searchTerm, setSearchTerm] = useState('')
   const [weatherData, setWeatherData] = useState([])
   const [city, setCity] = useState('Unknown location')
@@ -42,8 +42,8 @@ const App=() => {
       setWeatherIcon(`${process.env.REACT_APP_ICON_URL + data.list[0].weather[0]['icon']}@4x.png`)
 
     }catch(error){
-      console.log("An error occured: " +error)
-      
+      console.log("An error occured: " +error) 
+      setNoData("An Error has occured :(")    
     }
   }
 
@@ -53,25 +53,25 @@ const App=() => {
   }
 
   return (
-    <div className="bg-gray-800 flex items-center justify-center w-screen h-screen py-10">
-      <div className="flex w-3/4 min-h-full rounded-3xl shadow-lg m-auto bg-gray-100">
-        {/* form card section */ }
+    <div className="lg:flex items-center justify-center w-screen h-screen py-10">
+      <div className="lg:flex w-3/4 min-h-full rounded-3xl shadow-lg m-auto bg-gray-100">
+        {/* form section - background */ }
         <div className="form-container">
-          <div className="flex items-center justify-center">
-          <h3 className="my-auto mr-auto text-xl text-pink-800 font---bold shadow-md py-1 px-3 rounded--md bg-white bg-opacity-30">
+          <div className="lg:flex items-center justify-center">
+          <h3 className="my-auto mr-auto text-xl text-white font---bold shadow-md py-1 px-3 rounded--md bg-white bg-opacity-30">
             Weather Forecast
           </h3>
-          <div className="flex p-2 text-gray-100 bg-gray-600 bg-opacity-30 rounded-lg">
+          <div className="lg:flex p-2 text-gray-100 bg-gray-600 bg-opacity-30 rounded-lg">
             <i className="fa fa-map my-auto" aria--hidden="true"></i>
             <div className="text-right">
               <p className="font-semibold text-sm ml-2">{city}</p>
             </div>
           </div>
           </div>
-          <div className="flex flex-col items-center justify-center h-full">
-            <h1 className="text-white text-2xl"> The Only Weather Forecast App You Need!</h1>
+          <div className="lg:flex lg:flex-col items-center justify-center h-full">
+            <h1 className="text-white text-4xl"> Check your weather today!</h1>
             <hr className="h-1 bg-white w-1/4 rounded-full my-5"/>
-            <form noValidate onSubmit={handleSubmit} className="flex justify-center w-full">
+            <form noValidate onSubmit={handleSubmit} className="lg:flex justify-center w-full">
               <input type="text" 
                 placeholder="Enter location" 
                 className="relative rounded-xl py-2 px-3 w-2/3 bg-gray-300 bg-opacity-60 text-white placeholder-gray-200"
@@ -87,11 +87,11 @@ const App=() => {
             </form>
           </div>
         </div>
-        {/* info card section */}
-        <div className="w-2/4 p-5">             
-                <div className="flex flex-col my-10">
+        {/* weather info section */}
+        <div className="lg:w-2/4 p-5">             
+                <div className="lg:flex lg:flex-col my-10">
                   {weatherData.length === 0 ? 
-                  <div className="container p-4 flex items-center justify-center h-1 mb-auto">
+                  <div className="container p-4 lg:flex items-center justify-center h-1 mb-auto">
                     <h1 className="text-gray-300 text-4xl font-bp;d uppercase">{noData}</h1>
                   </div> :
                    <>
